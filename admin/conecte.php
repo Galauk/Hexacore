10fa9fcb4b2234ee81a05b9fe29dc3e71f113337
Angelo <angelo@hexacore.com.br>
2017-11-06 16:08:18 -0200

    Alterando conexão por PDO

--- admin/conecte.php
+++ admin/conecte.php
@@ -1,12 +1,14 @@
 <?
-$host='localhost'; // Local do Servidor
+$host='mysql857.umbler.com'; // Local do Servidor
 $user='hexacore_my'; //Nome de usuario do Sql
 $password='FK8{rGXzVG'; //Senha do servidor
 $database='hexacore_my'; // Base de dados
-$dsn = 'mysql:dbname='.$database.';host='.$host.';charset=UTF8';
+$dsn = 'mysql:dbname='.$database.';host='.$host;
 try {
-  $conexao = new PDO($dsn,$user,$password);
+  $conn = new PDO($dsn,$user,$password);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (Exception $e) {
+  echo $e->getMessage();
   die("Erro ao tentar se conectar");
 }
 ?>
