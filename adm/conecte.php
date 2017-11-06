114915d765abdc66e014f5af5c84893e8d66910d
Angelo <angelo@hexacore.com.br>
2017-11-06 16:05:57 -0200

    Verificando Conexão por PDO

--- adm/conecte.php
+++ adm/conecte.php
@@ -3,7 +3,7 @@ $host='mysql857.umbler.com'; // Local do Servidor
 $user='hexacore_my'; //Nome de usuario do Sql
 $password='FK8{rGXzVG'; //Senha do servidor
 $database='hexacore_my'; // Base de dados
-$dsn = 'mysql:dbname='.$database.';host='.$host.';charset=UTF8';
+$dsn = 'mysql:dbname='.$database.';host='.$host;
 try {
   $conn = new PDO($dsn,$user,$password);
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
