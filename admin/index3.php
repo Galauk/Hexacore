fccdcd95307e2781e3b57498eeba63779ba6adac
Angelo <angelo@hexacore.com.br>
2017-11-08 09:09:54 -0200

    index atualizado

--- admin/index3.php
+++ admin/index3.php
@@ -1,13 +1,14 @@
 <?php
+/*
 header('Content-Type: text/html; charset=utf-8');
 
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', "hexacore.com.br/admin");
-
 require 'core'.DS.'AutoLoad.php';
-require 'core/Controller.php';
-require 'core/Model.php';
-require 'core/View.php';
+*/
+require '/core/Controller.php';
+require '/core/Model.php';
+require '/core/View.php';
 
 /*$AutoLoad = new AutoLoad();
 
