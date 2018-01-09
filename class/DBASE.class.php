5daa5ae07dab096e230a8691faa785e6bcc7f2c7
Angelo Brito <angelo@hexacore.com.br>
2018-01-09 17:38:31 -0200

    adiciona o query a criação e ao try dentro do comando query

--- class/DBASE.class.php
+++ class/DBASE.class.php
@@ -13,20 +13,21 @@ class DBASE extends PDO {
         $this->password = "FK8{rGXzVG";
         $this->dbname = "hexacore_my";
         $this->dial = "mysql:host=".$this->host.";dbname=".$dbname;
-        try{
-            $this->conn = new PDO($this->dial, $this->user, $this->password);
-        }catch(PDOException $e){
-            echo "Falha na conexao:".$e->getMessage();
-        }
     }
 
     public function query($sql){
-        $res = $this->conn->prepare($sql);
+        $res = null;
+        try{
+            $this->conn = new PDO($this->dial, $this->user, $this->password);
+            $res = $this->conn->query($sql);
+        }catch(PDOException $e){
+            echo "Falha na conexao:".$e->getMessage();
+        }
         return $res;
     }
 
     public function auth($login, $password){
-        $stmt = $this->conn->query("SELECT * FROM admin WHERE login LIKE '".$login."' AND senha LIKE '".$password."'");
+        $stmt = $this->query("SELECT * FROM admin WHERE login LIKE '".$login."' AND senha LIKE '".$password."'");
         echo "<!--";
         var_dump($stmt);
         echo "-->";
