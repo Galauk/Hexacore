c0847ae1c33d9f7190c80d4f20d5d1c74b42a68e
Angelo Brito <angelo@hexacore.com.br>
2018-01-10 09:39:39 -0200

    removido a função query e adicionado o metodo dentro do proprio metodo auth

--- class/DBASE.class.php
+++ class/DBASE.class.php
@@ -13,25 +13,17 @@ class DBASE extends PDO {
         $this->password = "FK8{rGXzVG";
         $this->dbname = "hexacore_my";
         $this->dial = "mysql:host=".$this->host.";dbname=".$dbname;
-    }
-
-    public function query($sql){
-        $res = null;
         try{
             $conn = new PDO($this->dial, $this->user, $this->password);
-            $res = $conn->query($sql);
-
-            var_dump($conn);
-            die();
-
         }catch(PDOException $e){
             echo "Falha na conexao:".$e->getMessage();
         }
-        return $res;
     }
 
+
     public function auth($login, $password){
-        $stmt = $this->query("SELECT * FROM admin WHERE login LIKE '".$login."' AND senha LIKE '".$password."'");
+        $stmt = $this->prepare("SELECT * FROM admin WHERE login LIKE '".$login."' AND senha LIKE '".$password."'");
+        $stmt->execute();
         $n = $stmt->rowCount();
         if($n > 0){
             return true;
