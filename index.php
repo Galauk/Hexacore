0a2f62b24dee2be264ddb2032b668974a20ba89e
angelo@hexacore.com.br <angelo@hexacore.com.br>
2021-04-10 08:47:33 -0300

    display none

--- index.php
+++ index.php
@@ -8,7 +8,8 @@ include_once "bottom.php";
 
 $html->add(
 	$top.
-	$html->div('Seja bem vindo a Hexacore legal.',array('align'=>"center")).
+	$html->div('Seja bem vindo a Hexacore.',array('align'=>"center")).
+	$html->div('Seja bem vindo a Hexacore.',array('style'=>"display:none;")).
 	$bottom
 );
 $html->show();
