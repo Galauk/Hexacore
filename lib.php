1aa83f5bda701b7ec8500c9014985b98ec0f0a4b
ANGELO-NOTE\angelo <angelo@hexacore.com.br>
2023-08-22 18:54:40 -0300

    alteração de utilizador

--- lib.php
+++
@@ -1,9 +0,0 @@
-<?php
-	require_once 'class/BODY.class.php';
-	require_once 'class/DBASE.class.php';
-	/*
-	function my_autoload($class_name){
-		require_once "./class/" . $class_name . '.class.php';
-	}
-	*/
-?>
