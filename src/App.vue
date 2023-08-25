74a4d7f30b7ba2151ba19819c80fd77fd3043b81
ANGELO-NOTE\angelo <angelo@hexacore.com.br>
2023-08-25 10:09:32 -0300

    rebase para site voltar a ativo

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    Topo
+  </div>
+  <div>
+    Menu
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+</style>
